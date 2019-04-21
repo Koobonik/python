9b524d8657f869497f15c53a397763f84685cd81
Koobonik <kbi960130@naver.com>
2019-04-22 04:08:31 +0900

    내일 힘내자

--- practice/157p13.py
+++ practice/157p13.py
@@ -1,10 +1,11 @@
 a = "369"
+
 b = "693"
 
 strike = 0
 ball = 0
 
-for number in a:
+for number in list(a):
     if b.count(number) > 1:
         if b.find(number) != a.find(number):
             strike += 1
