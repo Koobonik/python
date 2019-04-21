504fb951efa3865567f9766989c7be3209f5b959
Koobonik <kbi960130@naver.com>
2019-04-22 01:20:03 +0900

    계속 파이썬 공부중

--- practice/stack.py
+++ practice/stack.py
@@ -13,9 +13,16 @@ print(word[::-1])
 
 # () -> 튜플
 # [] -> 리스트, 배열
-# {} -> 딕셔너리
+# {} -> 딕셔너리, 집
 
 s1 = set([1,2,3,4,5])
 s2 = set([4,5,6,7,8])
 print(s1 & s2)
 print(s1 - s2)
+a = {'name': 'pey', 'phone': '01022223333', 'birth': '1118'}
+print(a.keys())
+
+for i in a.keys():
+    print(i)
+print(a['name'])
+print(list( a.keys()))
