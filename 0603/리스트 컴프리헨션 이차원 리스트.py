3bbdf81ef3c4bd0848aefd866bc95bfa4771fc1e
Koobonik <kbi960130@naver.com>
2019-06-03 19:33:22 +0900

    리스트 컴프리헨션 응용

---
+++ 0603/리스트 컴프리헨션 이차원 리스트.py
@@ -0,0 +1,4 @@
+words = "The quick brown fox jumps over the lazy dog".split()
+print(words)
+stuff = [[w.upper(), w.lower(), len(w)] for w in words]
+print(stuff)
---
+++ 0603/리스트 컴프리헨션 필터링.py
@@ -0,0 +1,4 @@
+case_1 = ["A", "B", "C"]
+case_2 = ["D", "E", "A"]
+result = [i + j for i in case_1 for j in case_2 if not(i == j)]
+print(result)
